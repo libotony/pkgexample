0606aee1f64ed7aefe01bfe5d49ef278de5fd90f
tony <liboliqi@gmail.com>
2023-12-14 15:33:01 +0800

    initial commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/libotony/pkgexample
+
+go 1.19
+
+require (
+	github.com/btcsuite/btcd v0.0.0-20171128150713-2e60448ffcc6 // indirect
+	github.com/ethereum/go-ethereum v1.8.14 // indirect
+	github.com/vechain/thor v1.7.3 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+)
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	v "github.com/libotony/pkgexample/version"
+	"github.com/vechain/thor/thor"
+)
+
+func main() {
+	addr, err := thor.ParseAddress("0xdb54b4af8e040ce4f88b15afd6df5c00db42cfcf")
+	if err == nil {
+		fmt.Println(addr)
+	}
+	fmt.Printf("pkg example version %s\n", v.Version())
+}
